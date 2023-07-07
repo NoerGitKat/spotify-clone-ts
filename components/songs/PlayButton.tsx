fc77cf9dc50cb5d2a6f8f2b15491a83ee23047dc
Noer Paanakker <noerpaanakker@gmail.com>
2023-07-07 18:19:18 +0200

    Made PlayButton reusable

--- components/songs/PlayButton.tsx
+++ components/songs/PlayButton.tsx
@@ -1,10 +1,30 @@
-import React, { FC } from "react";
+import React, { FC, ReactNode } from "react";
+import { IconType } from "react-icons";
 import { FaPlay } from "react-icons/fa";
+import { twMerge } from "tailwind-merge";
 
-const PlayButton: FC<object> = () => {
+interface PlayButtonProps {
+  className?: string;
+  Icon?: IconType;
+  size?: number;
+  handleClick: () => void;
+}
+
+const PlayButton: FC<PlayButtonProps> = ({
+  className,
+  Icon = FaPlay,
+  handleClick,
+  size
+}) => {
   return (
-    <button className="transition opacity-0 rounded-full flex items-center bg-green-500 p-4 drop-shadow-md translate translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110">
-      <FaPlay className="text-black" />
+    <button
+      onClick={handleClick}
+      className={twMerge(
+        "transition opacity-0 rounded-full flex bg-green-500 p-4 drop-shadow-md translate translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110",
+        className
+      )}
+    >
+      <Icon className="text-black" size={size} />
     </button>
   );
 };
